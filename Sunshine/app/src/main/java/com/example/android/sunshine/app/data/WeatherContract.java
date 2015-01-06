88c6c24b1e402169d2aec6a729472b99241fe22d
imadityab <imadityab@gmail.com>
2015-01-05 21:26:04 -0500

    Added SQLite table creation code
    Added SQLite table creation code along with JUnit Test suite for db and
    table creation use cases.

--- Sunshine/app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
+++ Sunshine/app/src/main/java/com/example/android/sunshine/app/data/WeatherContract.java
@@ -97,10 +97,20 @@ public class WeatherContract {
 
     public static final class LocationEntry implements BaseColumns {
         /**
-         * TODO YOUR CODE BELOW HERE FOR QUIZ
+         * DONE: HERE FOR QUIZ
          * QUIZ - 4a - Columns
          * https://www.udacity.com/course/viewer#!/c-ud853/l-1639338560/e-1633698595/m-1633698597
          **/
+        public static final String TABLE_NAME = "location";
+
+
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+
+        public static final String COLUMN_CITY_NAME = "city_name";
+
+        public static final String COLUMN_COORD_LAT = "coord_lat";
+
+        public static final String COLUMN_COORD_LONG = "coord_long";
 
         /* TODO Uncomment for
         4b - Adding ContentProvider to our Contract
